55cad16cd70f8b85eda83657fdd7b5295f113162
Lee-Po-Tsung <b111201126@gapps.ntust.edu.tw>
2025-06-25 04:47:12 +0000

    測試

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,30 @@
 import streamlit as st
+import os
+import sys
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+def hello():
+    os.system(sys.executable + " --version")
+
+def randomSong():
+    ...
+
+def popList():
+    ...
+
+def allList():
+    ...
+
+def settingPage():
+    ...
+
+def showSong(song):
+    ...
+
+st.navigation([
+    st.Page(hello, title='首頁', icon=':material/house:', default=True),
+    st.Page(allList, title='全部音樂', icon='🎵', url_path='song-list'),
+    st.Page(popList, title='熱門音樂', icon='🔥', url_path='pop-song-list'),
+    st.Page(randomSong, title='隨機歌曲', icon='🔀', url_path='random-song'),
+    st.Page(settingPage, title='設定', icon='⚙', url_path='setting')
+]).run()
+
